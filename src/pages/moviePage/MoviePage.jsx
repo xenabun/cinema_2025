cb2f22750f74b0cbe4fa26cc9e2a0eb50d2b8430
xenabun <92882911+xenabun@users.noreply.github.com>
2025-12-12 15:53:50 +0300

    movie page api

--- src/pages/moviePage/MoviePage.jsx
+++ src/pages/moviePage/MoviePage.jsx
@@ -1,68 +1,77 @@
 import { Descriptions, Flex, Typography, Tag } from "antd";
+import { useParams } from "react-router-dom";
 import "./MoviePage.css";
+import { useEffect, useState } from "react";
+import { getMovie } from "../../utils/requests";
 
 const MoviePage = () => {
-  const movie = {
+  const { id } = useParams();
+  
+  const [movie, setMovie] = useState({
     id: 1,
     title: {
-      ru: "Дюна: Часть вторая",
-      en: "Dune: Part Two",
+      ru: "Загрузка",
+      en: "Loading",
     },
-    rating: 8.2,
+    rating: "?",
     details: {
-      year: 2024,
-      director: "Дени Вильнёв",
-      screenwriter: "Дени Вильнёв",
-      genres: ["драма", "боевик", "фантастика"],
-      country: "США",
-      description:
-        "Герцог Пол Атрейдес присоединяется к фрименам, чтобы стать Муад'Дибом, одновременно пытаясь остановить наступление войны.",
+      year: "?",
+      director: "?",
+      screenwriter: "?",
+      genres: [],
+      country: "?",
+      description: "?",
     },
-    poster:
-      "https://avatars.mds.yandex.net/get-kinopoisk-image/4486454/34ecdec6-c76f-4cd7-818c-905803184ed8/orig",
-    actors: [
-      "Тимоти Шаламе",
-      "Зендая",
-      "Ребекка Фергюсон",
-      "Хавьер Бардем",
-      "Остин Батлер",
-    ],
-  };
-  const items = [
-    { label: "год", children: movie.details.year },
-    {
-      label: "режиссер",
-      children: movie.details.director,
-    },
-    {
-      label: "сценарист",
-      children: movie.details.screenwriter,
-    },
-    {
-      label: "жанр",
-      children: (
-        <>
-          {movie.details.genres.map((genre) => (
-            <Tag color="#FF7A85" key={genre}>
-              {genre}
-            </Tag>
-          ))}
-        </>
-      ),
-    },
-    {
-      label: "страна",
-      children: movie.details.country,
-    },
-    {
-      label: "описание",
-      children: movie.details.description,
-      span: 3,
-    },
-  ];
+    poster: "",
+    actors: [],
+  });
+  const [items, setItems] = useState([]);
+
+  useEffect(() => {
+	getMovie(id)
+	  .then((res) => {
+		setMovie(res.data.data);
+	  })
+	  .catch((e) => console.warn(e));
+  }, []);
+
+  useEffect(() => {
+	setItems([
+		{ label: "год", children: movie.details.year },
+		{
+		label: "режиссер",
+		children: movie.details.director,
+		},
+		{
+		label: "сценарист",
+		children: movie.details.screenwriter,
+		},
+		{
+		label: "жанр",
+		children: (
+			<>
+			{movie.details.genres.map((genre) => (
+				<Tag color="#FF7A85" key={genre}>
+				{genre}
+				</Tag>
+			))}
+			</>
+		),
+		},
+		{
+		label: "страна",
+		children: movie.details.country,
+		},
+		{
+		label: "описание",
+		children: movie.details.description,
+		span: 3,
+		},
+	]);
+  }, [movie]);
 
   return (
-    <div id="movie-page">
+    movie !== undefined ? <div id="movie-page">
       <div className="content-movie-container">
         <Flex gap={30} align="center">
           <Typography.Title className="movie-title" level={4}>
@@ -89,7 +98,7 @@ const MoviePage = () => {
         </Flex>
       </div>
       <div className="content-movie-container"></div>
-    </div>
+    </div> : <></>
   );
 };
 
